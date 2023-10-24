74e0c9c3397fbee2e191df1da0eeb9ba21d331d9
Callum McGregor <callum.mcgregor@capgemini.com>
2023-10-24 09:46:12 +0100

    rich terminal testing

--- rich_terminal.py
+++ rich_terminal.py
@@ -7,7 +7,7 @@ from rich.panel import Panel
 console = Console()
 
 # Create a Panel with a title and content
-panel = Panel.fit("This is a colored box with rounded edges!", title="Rich Box Example", border_style="blue")
+panel = Panel.fit("this is a colored box with rounded edges!", title="text box example", border_style="blue")
 
 # Display the panel
 console.print(panel)
